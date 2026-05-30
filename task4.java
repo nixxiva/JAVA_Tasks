5b7de196e5825d2a678cdd52d7392fe028b2931f
Sarah Angelica Bustinera <sarahbustinera24@gmail.com>
2026-05-30 00:49:50 +0000

    Task 4  - Palindrome

---
+++ task4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public static void main(String[] args){
+
+    Scanner s = new Scanner(System.in);
+    System.out.println("Enter the word to check palindrome : ");
+    String word = s.nextLine();
+
+    StringBuilder w = new StringBuilder("madam");
+
+    if (word.equalsIgnoreCase(w.reverse().toString()))
+        System.out.println("The input string is a palindrome.");
+    else 
+        System.out.println("The input string is not a palindrome.");
+
+    s.close();
+}
