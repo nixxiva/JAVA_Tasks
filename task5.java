2ac7d1df5c0e5dabbfba7b07402737bcf82a6b6a
Sarah Angelica Bustinera <sarahbustinera24@gmail.com>
2026-06-03 03:58:55 +0000

    Tofix: Highest number

---
+++ task5.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public static void main(String[] args){
+
+    Scanner s = new Scanner(System.in);
+
+    int[] arr;
+
+    System.out.println("Enter 1st number : ");
+    
+    System.out.println("Enter 2nd number : ");
+    int two = s.nextInt();
+
+    System.out.println("Enter 3rd number : ");
+    int three = s.nextInt();
+
+    if
+
+}
