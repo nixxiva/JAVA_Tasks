2b2324a802424c43a86f0322e5ea8f757d2769b4
Sarah Angelica Bustinera <sarahbustinera24@gmail.com>
2026-06-03 03:45:57 +0000

    Task 6 - Iterating Array

--- task4.java
+++ task4.java
@@ -8,10 +8,14 @@ public static void main(String[] args){
 
     StringBuilder w = new StringBuilder("madam");
 
-    if (word.equalsIgnoreCase(w.reverse().toString()))
-        System.out.println("The input string is a palindrome.");
-    else 
-        System.out.println("The input string is not a palindrome.");
+    // if (word.equalsIgnoreCase(w.reverse().toString()))
+    //     System.out.println("The input string is a palindrome.");
+    // else 
+    //     System.out.println("The input string is not a palindrome.");
+
+    word.equalsIgnoreCase(w.reverse().toString()) ?
+        System.out.println("The input string is a palindrome.") : System.out.println("The input string is not a palindrome.")
+
 
     s.close();
 }
