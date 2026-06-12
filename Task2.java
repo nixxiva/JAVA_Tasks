f19425bc23d27680afb35b8ea099d304da8fac15
Sarah Angelica Bustinera <sarahbustinera24@gmail.com>
2026-06-12 09:34:55 +0000

    H3110 w0rld 2.0 true

---
+++ Task2.java
@@ -0,0 +1,18 @@
+public class Task2 {
+	
+	public static void main(String[] args){
+		
+		byte zero = 0;
+		char h = 'H', w = 'w', r = 'r', l = 'l', d = 'd';
+		int ell = 311;
+		float ver = 2.0f;
+		boolean y = true;
+		
+		String output = "" + h + ell + zero + " " + w + zero + r + l + d + " " + ver + " " + y; 
+
+		System.out.println(output);
+		
+	}
+
+
+}
