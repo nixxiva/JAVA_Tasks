c0093ccb772b92538a4ae6dff34abdafa667be31
Sarah Angelica Bustinera <sarahbustinera24@gmail.com>
2026-06-25 04:38:19 +0000

    Fix: Syntax

--- task4.java
+++ task4.java
@@ -3,19 +3,18 @@ import java.util.Scanner;
 public static void main(String[] args){
 
     Scanner s = new Scanner(System.in);
-    System.out.println("Enter the word to check palindrome : ");
+    System.out.print("Enter the word to check palindrome : ");
     String word = s.nextLine();
 
-    StringBuilder w = new StringBuilder("madam");
+    StringBuilder w = new StringBuilder(word);
 
     // if (word.equalsIgnoreCase(w.reverse().toString()))
     //     System.out.println("The input string is a palindrome.");
     // else 
     //     System.out.println("The input string is not a palindrome.");
 
-    word.equalsIgnoreCase(w.reverse().toString()) ?
-        System.out.println("The input string is a palindrome.") : System.out.println("The input string is not a palindrome.")
-
+    System.out.println(word.equalsIgnoreCase(w.reverse().toString()) ?
+        "The input string is a palindrome." : "The input string is not a palindrome.");
 
     s.close();
 }
