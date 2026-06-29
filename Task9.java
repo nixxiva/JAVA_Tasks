91d1a6c6761adda99c87c1c409adc1a83a44e1bb
Sarah Angelica Bustinera <sarahbustinera24@gmail.com>
2026-06-29 04:14:47 +0000

    feat: implement math operations using static methods and Math static imports.v2

--- Task9.java
+++ Task9.java
@@ -6,41 +6,37 @@ public class Task9{
         
         // ADD
         System.out.println("ADDITION:");
-        System.out.println(add(1,2)); //static
-        System.out.println(Math.addExact(10, 23)); //import method
+        System.out.println(add(1,2));
 
         // SUBTRACT
         System.out.println("SUBTRACTION:");
         System.out.println(subtract(10, 7));
-        System.out.println(Math.subtractExact(24, 2));
 
         // MULTIPLY
         System.out.println("MULTIPLICATION:");
         System.out.println(subtract(34, 35));
-        System.out.println(Math.multiplyExact(24, 24));
 
         // DIVIDE
         System.out.println("DIVISION:");
         System.out.println(divide(13, 5));
-        System.out.println(Math.divideExact(9, 4));
         
     }
 
     // Static methods
     public static int add(int a, int b){
-        return a + b;
+        return addExact(a, b);
     }
     
     public static int subtract(int a, int b){
-         return a - b;
+         return subtractExact(a, b);
     }
     
     public static int multiply(int a, int b){
-        return a * b;
+        return multiplyExact(a, b);
     }
     
     public static float divide(int a, int b){
-        return a / b;
+        return divideExact(a, b);
     }
 
 }
